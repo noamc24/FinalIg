23d0667c554244cbd3031237ee9d02d51f46b9cc
noamc24 <noamc24@gmail.com>
2025-07-23 17:32:43 +0300

    feat: create uploads directory if it doesn't exist

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
