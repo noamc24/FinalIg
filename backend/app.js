9ba8abbfd1dd05cc9b98d60bdcde8df14f6a9b72
Elad Abutbul <eladabutbul61@gmail.com>
2025-09-02 19:09:27 +0300

    change things to look good

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,30 @@
-const { connectDB } = require("./config/db");
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const app = express();
-const PORT = 3000;
+const { connectDB } = require("./config/db");
 
-// Middlewares
-app.use(express.json({ limit: "1000mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1000mb" }));
-app.use(express.static(path.join(__dirname, "html")));
-app.use("/css", express.static(path.join(__dirname, "css")));
-app.use("/js", express.static(path.join(__dirname, "js")));
-app.use("/assets", express.static(path.join(__dirname, "assets")));
-app.use("/uploads", express.static("uploads"));
-app.use(cors());
-
-// Mongo connection
-mongoose
-  .connect("mongodb://localhost:27017/new-users")
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ Mongo Error:", err));
-
-// API Routes
 const authRouter = require("./routes/authRoute");
 const usersRouter = require("./routes/userRoute");
 const postsRouter = require("./routes/postsRoute");
 const groupsRouter = require("./routes/groupRoute");
 const postsExtrasRouter = require("./routes/postsExtrasRoute");
 
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+// API
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/groups", groupsRouter);
 app.use("/api/post-extras", postsExtrasRouter);
 
-// 👇 זה חשוב מאוד שיהיה כאן (לפני ה-listen)
-app.get("/profile/:username", (req, res) => {
-  res.sendFile(path.join(__dirname, "html", "profile.html"));
-});
 
-// Start the server
+
+
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`🚀 http://localhost:${PORT}`));
 });
-app.use(express.static("assets"));
