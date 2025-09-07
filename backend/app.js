a31f15d4416c65027430f07a715c7693784e3b28
noamc24 <noamc24@gmail.com>
2025-09-07 20:26:35 +0300

    Refactor post creation comments and adjust image dimensions in dark mode

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(cors({
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+app.use('/frontend', express.static(path.join(__dirname, '..', 'frontend')));
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/posts", postsRouter);
